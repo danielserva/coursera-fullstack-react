0fcdf1d1c1ef83e83065a3ef17015cddb73e2b4d
Sopa <danielserva@gmail.com>
2020-12-20 18:47:31 +0100

    Post comments

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -26,7 +26,7 @@ class CommentForm extends Component {
 
   handleSubmit(values) {
     this.toggleModal();
-    this.props.addComment(this.props.dishId, values.rating, values.author, values.comment);
+    this.props.postComment(this.props.dishId, values.rating, values.author, values.comment);
   }
 
   render() {
@@ -87,7 +87,7 @@ class CommentForm extends Component {
   }
 }
 
-function RenderComments({comments, addComment, dishId}){
+function RenderComments({comments, postComment, dishId}){
   if (comments != null){
     const commentList = comments.map((commentNode) =>
       <li key={commentNode.id}>
@@ -98,7 +98,7 @@ function RenderComments({comments, addComment, dishId}){
         <ul className="list-unstyled text-left">{commentList}</ul>
         <CommentForm isModalOpen={false} 
           dishId={dishId}
-          addComment={addComment} />
+          postComment={postComment} />
         </div>);
   } else {
     return (
@@ -160,7 +160,7 @@ const DishDetail = (props) => {
         <div className="row">
             <RenderDish dish={props.dish} />
             <RenderComments comments={props.comments}
-              addComment={props.addComment}
+              postComment={props.postComment}
               dishId={props.dish.id} />
         </div>
       </div>
